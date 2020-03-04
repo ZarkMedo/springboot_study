412cd0d0c47d5243bb3c82bd711da91637837809
git_by_medo_mac <medo_mac@github.com>
2020-03-04 12:18:48 +0800

    freemarker 字典传参以及incloud的使用

--- freemarker/src/main/java/com/medo/freemarker/controller/IndexController.java
+++ freemarker/src/main/java/com/medo/freemarker/controller/IndexController.java
@@ -4,9 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * @ClassName: IndexController
@@ -40,6 +38,32 @@ public class IndexController {
         modelMap.addAttribute("list",list);
         return "f2";
     }
+    @GetMapping("/f3")
+    public String f3(ModelMap modelMap) {
 
+        System.out.println("传入map");
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "老习");
+        map.put("gender", "男");
+        map.put("like", "女");
+        map.put("age", "64");
+        map.put("address", "中南海");
+        modelMap.addAttribute("map",map);
+        return "f3";
+    }
 
+    @GetMapping("/f4")
+    public String f4(ModelMap modelMap) {
+
+        System.out.println("if判断");
+        ArrayList<String> list = new ArrayList<>();
+        Collections.addAll(list, "老王", "老李", "老刘", "老妈子", "老牛", "老冰");
+        modelMap.addAttribute("list",list);
+        return "f4";
+    }
+    @GetMapping("/f5")
+    public String f5(ModelMap modelMap) {
+
+        return "f5";
+    }
 }
